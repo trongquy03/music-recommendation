3054f0ca2ad12da47bba95c50f3e0335e80b32e6
Trong Quy <trongquy1628@gmail.com>
2025-04-01 03:49:36 +0700

    create project base

---
+++ src/test/java/vn/trongquy/MusicWebApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.trongquy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
